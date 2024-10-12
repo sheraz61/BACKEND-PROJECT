3346f6ca86ba28f52e186328f0555d5c710a8cb5
sheraz61 <hsheraz271@gmail.com>
2024-10-12 23:40:42 +0500

    update cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,62 +1,55 @@
 import { v2 as cloudinary } from "cloudinary";
-import {extractPublicId} from "cloudinary-build-url"
+import { extractPublicId } from "cloudinary-build-url"
 import fs from "fs";
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        //upload on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        //file has been uploaded successfully
+        // console.log('file uploaded on cloudinary',response.url)
+        fs.unlinkSync(localFilePath)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove localy save tem file as the upload failed
+        return null
+    }
+}
 
-
-
-
-
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key:process.env. CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET
-    });
-    
-
-    const uploadOnCloudinary=async(localFilePath )=>{
-        try {
-            if(!localFilePath) return null;
-            //upload on cloudinary
-          const response= await cloudinary.uploader.upload(localFilePath,{
-                resource_type:'auto'
-            })
-            //file has been uploaded successfully
-            // console.log('file uploaded on cloudinary',response.url)
-            fs.unlinkSync(localFilePath)
-            return response
-        } catch (error) {
-            fs.unlinkSync(localFilePath) //remove localy save tem file as the upload failed
+const deleteInCloudinary = async (fileUrl) => {
+    try {
+        if (!fileUrl) {
             return null
         }
-    }
-
-    const deleteInCloudinary = async (fileUrl)=>{
-        try {
-            if (!fileUrl) {
-                return null
-            }
-            const publicId = extractPublicId(fileUrl)
-            if (!publicId) {
-                return null
-            }
-    
-            let resourceType = "image"; // Default to image
-            if (fileUrl.match(/\.(mp4|mkv|mov|avi)$/)) {
-                resourceType = "video";
-            } else if (fileUrl.match(/\.(mp3|wav)$/)) {
-                resourceType = "raw"; // For audio or other file types
-            }
-    
-            const res = await cloudinary.uploader.destroy(publicId,{resource_type:resourceType})
-            return res;
-        } catch (error) {
-            return null;
+        const publicId = extractPublicId(fileUrl)
+        if (!publicId) {
+            return null
         }
-    }
- 
 
-    export {
-        uploadOnCloudinary,
-        deleteInCloudinary
+        let resourceType = "image"; // Default to image
+        if (fileUrl.match(/\.(mp4|mkv|mov|avi)$/)) {
+            resourceType = "video";
+        } else if (fileUrl.match(/\.(mp3|wav)$/)) {
+            resourceType = "raw"; // For audio or other file types
+        }
+
+        const res = await cloudinary.uploader.destroy(publicId, { resource_type: resourceType })
+        return res;
+    } catch (error) {
+        return null;
     }
+}
+
+
+export {
+    uploadOnCloudinary,
+    deleteInCloudinary
+}
